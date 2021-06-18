c5263d48361a0a2688db9bf49925d3334dac21cc
Lenovo <Lenovo@LAPTOP-EJVTEQ8K>
2021-06-18 12:11:52 +0530

    the first one update

---
+++ SumProgram/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello this is test work");
+		System.out.println("The sum of two number is :");
+		int x =20;
+		int y =30;
+		int sum =x+y;
+		System.out.println("The sum of x and y is ="+sum);
+	}
+
+}
